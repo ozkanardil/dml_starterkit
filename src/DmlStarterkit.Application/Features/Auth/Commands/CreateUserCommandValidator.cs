57ca779866ca0b5a3670825dbd042fdc74715dbd
Ozkan <ozkanardil@gmail.com>
2024-11-09 12:28:57 +0100

    Fluent validation integration

---
+++ src/DmlStarterkit.Application/Features/Auth/Commands/CreateUserCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace DmlStarterkit.Application.Features.Auth.Commands
+{
+    public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+    {
+        public CreateUserCommandValidator()
+        {
+            RuleFor(x => x.firstName)
+                .NotEmpty().WithMessage("Firt name must not be empty.")
+                .Length(2,20).WithMessage("First name must consist of at least two letters.");
+
+            RuleFor(x => x.lastName)
+                .NotEmpty().WithMessage("Last name must not be empty.")
+                .Length(2, 20).WithMessage("Last name must consist of at least two letters."); ;
+
+            RuleFor(x => x.password)
+                .NotEmpty().WithMessage("Password must not be empty.")
+                .Length(4, 8).WithMessage("Password must be at least 4 and maximum 8 characters.");
+
+            RuleFor(x => x.email)
+                .NotEmpty().WithMessage("E-mail must not be empty.")
+                .EmailAddress().WithMessage("Enter a valid e-mail address.");
+        }
+    }
+}
---
+++ src/DmlStarterkit.Application/Features/UserRole/Command/DeleteUserRoleCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace DmlStarterkit.Application.Features.UserRole.Command
+{
+    public class DeleteUserRoleCommandValidator : AbstractValidator<DeleteUserRoleCommand>
+    {
+        public DeleteUserRoleCommandValidator()
+        {
+            RuleFor(x => x.Id).GreaterThan(0).WithMessage("Id must be greater than zero.");
+        }
+    }
+}
