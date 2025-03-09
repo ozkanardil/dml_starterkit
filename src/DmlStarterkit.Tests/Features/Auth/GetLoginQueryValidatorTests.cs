88599f112e29db3721eb8c4274427cf71b51ee69
Ozkan <ozkanardil@gmail.com>
2025-03-09 20:43:32 +0100

    Unit test project is added to the solution

---
+++ src/DmlStarterkit.Tests/Features/Auth/GetLoginQueryValidatorTests.cs
@@ -0,0 +1,71 @@
+﻿using DmlStarterkit.Application.Features.Auth.Queries;
+using FluentValidation.TestHelper;
+using Xunit;
+
+namespace DmlStarterkit.Tests.Features.Auth
+{
+    public class GetLoginQueryValidatorTests
+    {
+        private readonly GetLoginQueryValidator _validator;
+
+        public GetLoginQueryValidatorTests()
+        {
+            _validator = new GetLoginQueryValidator();
+        }
+
+        [Fact]
+        public void Should_Pass_When_Email_Is_Valid()
+        {
+            // Arrange
+            var query = new GetLoginQuery
+            {
+                Email = "test@example.com",
+                Password = "ValidPassword123!"
+            };
+
+            // Act
+            var result = _validator.TestValidate(query);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.Email);
+        }
+
+        [Theory]
+        [InlineData("testexample.com")]
+        [InlineData("test@")]
+        [InlineData("userdomain..com")]
+        public void Should_Not_Pass_When_Email_Is_Invalid(string invalidEmail)
+        {
+            // Arrange
+            var query = new GetLoginQuery
+            {
+                Email = invalidEmail,
+                Password = "ValidPassword123!"
+            };
+
+            // Act
+            var result = _validator.TestValidate(query);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+
+        [Fact]
+        public void Should_Pass_When_Email_Is_CaseInsensitive()
+        {
+            // Arrange
+            var query = new GetLoginQuery
+            {
+                Email = "Test@Example.COM",
+                Password = "ValidPassword123!"
+            };
+
+            // Act
+            var result = _validator.TestValidate(query);
+
+            // Assert
+            result.ShouldNotHaveValidationErrorFor(x => x.Email);
+        }
+    }
+}
---
+++ src/DmlStarterkit.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace DmlStarterkit.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
